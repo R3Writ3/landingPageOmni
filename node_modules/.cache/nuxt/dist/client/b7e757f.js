3e5ca5a687299393927c8606c99517ca9ada0019
Rafly Hersa Pratama <53065877+R3Writ3@users.noreply.github.com>
2021-11-12 10:44:09 +0700

    update responsive

---
+++ node_modules/.cache/nuxt/dist/client/b7e757f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{465:function(n,e,t){"use strict";t.r(e);var l=t(20),c=t(22),r=t.n(c),o=t(456),w=t(460),component=Object(l.a)({},(function(){var n=this.$createElement,e=this._self._c||n;return e("v-app",[e("v-main")],1)}),[],!1,null,null,null);e.default=component.exports;r()(component,{VApp:o.a,VMain:w.a})}}]);
